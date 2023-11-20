a3d6ccd23fed5e58b96a2779a1c25dbee3473070
larltr <larltr17@gmail.com>
2023-11-20 22:33:57 +0600

    created a file for styles to use for texts

---
+++ app/src/main/java/com/angelika/android5lesson_5/ui/theme/Font.kt
@@ -0,0 +1,11 @@
+package com.angelika.android5lesson_5.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import com.angelika.android5lesson_5.R
+
+val Rubik = FontFamily(
+    Font(R.font.rubik_bold, FontWeight.Bold, FontStyle.Normal),
+)
